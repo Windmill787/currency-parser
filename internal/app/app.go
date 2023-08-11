b48223eb0a0e14d2e75ec7d1fd81cc82e157c25f
Stolyarov Max <Windmill78max@gmail.com>
2023-08-11 20:45:30 +0300

    Repository removed, instructions added

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,14 @@
 package app
 
-import "fmt"
-
 func NewApp() {
-	fmt.Println("Application initialized")
+	//create http client
+	//create service that depends on client
+	//use service to execute requests using http client
+
+	//parse currensy rate for UAH -> USD
+	//notify used to telegram
+	//save this current rate to ram
+	//set timeout for 1 hour
+	//parse again
+	//if rate is changed notify client, add the change amount to message
 }
