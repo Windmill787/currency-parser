70b5227a4bf40d517ff784172a953b9ad2cc3123
Stolyarov Max <Windmill78max@gmail.com>
2023-08-12 15:51:54 +0300

    Created custom http client, added test http request using this client

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,5 +1,13 @@
 package app
 
+import (
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/Windmill787/currency-parser/internal/client"
+)
+
 func NewApp() {
 	//create http client
 	//create service that depends on client
@@ -11,4 +19,21 @@ func NewApp() {
 	//set timeout for 1 hour
 	//parse again
 	//if rate is changed notify client, add the change amount to message
+	fmt.Println("Client")
+
+	client := client.NetClient()
+
+	resp, err := client.Get("http://vseosvita.ua")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	_, err = io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Println(string(body))
+	fmt.Println("Response status: ", resp.StatusCode)
 }
--- internal/client/client.go
+++ internal/client/client.go
@@ -1,5 +1,33 @@
 package client
 
-func NetClient() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
 
+type loggerRoundTripper struct {
+	writer io.Writer
+	next   http.RoundTripper
+}
+
+func (l *loggerRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	fmt.Fprintf(l.writer, "[%s] %s %s\n", time.Now().Format(time.ANSIC), r.Method, r.URL)
+	return l.next.RoundTrip(r)
+}
+
+func NetClient() *http.Client {
+	return &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			fmt.Println("Redirect")
+			return nil
+		},
+		Transport: &loggerRoundTripper{
+			next:   http.DefaultTransport,
+			writer: os.Stdout,
+		},
+		Timeout: time.Second * 3,
+	}
 }
